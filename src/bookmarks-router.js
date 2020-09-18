1f056ffecb7695d884ae642b406a3d73bb0cdd1c
Theo McCauley <TheoMakaurii@gmail.com>
2020-09-18 15:17:55 -0400

    added logging info/ error

--- src/bookmarks-router.js
+++ src/bookmarks-router.js
@@ -3,11 +3,13 @@ const {
     v4: uuid
 } = require('uuid')
 const validation = require('./validation')
-const bookmarks = require('./store')
+const bookmarks = require('./store');
+const logger = require('../logger');
 const bookmarkRouter = express.Router()
 const bodyParser = express.json()
 
 bookmarkRouter.get('/bookmarks', bodyParser, validation, (req, res) => {
+    logger.info(`Got bookmarks`)
     res.json(bookmarks)
 })
 
@@ -17,10 +19,12 @@ bookmarkRouter.get('/bookmarks/:id', bodyParser, validation, (req, res) => {
     } = req.params;
     const index = bookmarks.findIndex(bookmark => bookmark.id === id);
     if (index === -1) {
+        logger.error(`Bookmark with id ${id} not found.`)
         return res
             .status(404)
             .send('Bookmark not found!');
     }
+    logger.info(`Got bookmarks with id ${id}`)
     res.json(bookmarks[index])
 })
 
@@ -33,12 +37,14 @@ bookmarkRouter.post('/bookmarks', bodyParser, validation, (req, res) => {
     } = req.body;
 
     if (!title) {
+        logger.error(`Title is required!`)
         return res
             .status(400)
             .send('No title was included in new bookmark!')
     }
 
     if (!url) {
+        logger.error(`Valid URL is required!`)
         return res
             .status(400)
             .send('No URL was included in new bookmark!')
@@ -54,7 +60,7 @@ bookmarkRouter.post('/bookmarks', bodyParser, validation, (req, res) => {
     }
 
     bookmarks.push(bookmark)
-
+    logger.info(`Bookmark with is ${id} created`)
     res
         .status(201)
         .location(`http://localhost:8000/bookmarks/`)
@@ -67,13 +73,14 @@ bookmarkRouter.delete('/bookmarks/:id', bodyParser, validation, (req, res) => {
     } = req.params;
     const index = bookmarks.findIndex(bookmark => bookmark.id === id);
     if (index === -1) {
+        logger.error(`Can't delete. Bookmark with id ${id} not found.`)
         return res
             .status(404)
             .send('Bookmark not found!');
     }
 
     bookmarks.splice(index, 1);
-
+    logger.info(`Deleted bookmark with id ${id}`)
     res.json(bookmarks)
 
     res
